6f26a61de4142aa4dcf92a565561e94985fe8682
Sune Radich Christensen <sykokaj@gmail.com>
2013-09-11 14:02:01 +0200

    More work done

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
 /*global jQuery*/
 
-(function($){
+(function ($) {
 
     /* MODELS */
-    var SpacesModel = Backbone.Model.extend({});
+    var OrganisationModel = Backbone.Model.extend({
+    });
 
-    var SpacesCollection = Backbone.Collection.extend({
-        model: SpacesModel,
+    var OrganisationCollection = Backbone.Collection.extend({
+        model: OrganisationModel,
         url: 'data/data.json',
-        parse: function(response) {
+        parse: function (response) {
             var rtn = {};
             rtn.areas = response;
             return rtn;
         }
     });
 
-    var SpacesView = Backbone.View.extend({
+    var OrganisationView = Backbone.View.extend({
         el: ".menu > div input",
-        //template: _.template("<h1><%= name %></h1>"),
-        template_area: [
+        template: [
             '<div>',
             '<ol>',
             '{{#areas}}',
@@ -39,72 +39,34 @@
             '</ol>',
             '</div>'
         ].join(''),
-        template: (function(){
 
-            //Partial template, to display an area
-            var template_area = [
-                '<div>',
-                '<ol>',
-                '{{#areas}}',
-                '<li>',
-                '<div>',
-                '<img src="{{image.thumbnail_link}}" alt="{{hosted_by_humanized_name}}">',
-                '</div>',
-                '<div class="areaContainer">',
-                '<a href="{{url}}">{{{name}}}</a>',
-                '<ol>',
-                '{{#spaces}}',
-                '<li><a href="{{url}}">{{{name}}}</a></li>',
-                '{{/spaces}}',
-                '</ol>',
-                '</div>',
-                '</li>',
-                '{{/areas}}',
-                '</ol>',
-                '</div>'
-            ].join('');
-
-            var template = [
-                '<div>',
-                '<span class="tab"></span>',
-                '<input type="text">',
-                '{{> template_area}}',
-                '</div>'].join('');
-
-            //Compile partial template for faster reference and usage in the main template
-            return Mustache.compile(template);
-        }()),
+        //The html that will be appended to the DOM
         output: "",
+        events: {
+            'click': 'inputKeyup'
+        },
+        inputKeyup: function () {
+            console.log('filt', this);
 
-        initialize: function() {
+        },
+        initialize: function () {
             var self = this;
-            this.collection = new SpacesCollection();
-
-            console.log('self', this);
-            this.collection.fetch().complete(function(){
+            this.collection = new OrganisationCollection();
+            this.collection.fetch().complete(function () {
                 self.render();
             });
         },
-
-        renderCollection: function(){
+        render: function () {
             var self = this;
-            this.collection.each(function(model) {
-                self.output += Mustache.render(self.template_area, model.attributes);
-                //console.log(self.output);
+            this.collection.each(function (model) {
+                self.output += Mustache.render(self.template, model.attributes);
             });
 
             self.$el.after(self.output);
-            //console.log('Rendering collection', this.collection);
-        },
-
-        render: function() {
-            //this.$el.html(this.template({"name": "Dummy"}));
-            //this.$el.html(Mustache.render(this.template, config))
-            this.renderCollection();
         }
     });
 
-    var spacesview = new SpacesView();
+    var spacesview = new OrganisationView();
 
 }(jQuery));
 
