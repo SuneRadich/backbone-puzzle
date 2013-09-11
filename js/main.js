e9707406daeffaf5cad64cb2fbe7de9415653eab
Sune Radich Christensen <sykokaj@gmail.com>
2013-09-11 16:33:02 +0200

    More work done

--- js/main.js
+++ js/main.js
@@ -3,15 +3,39 @@ var podio = podio || {};
 (function ($) {
 
     podio.SpaceModel = Backbone.Model.extend({
-        initialize: function () {}
+        initialize: function () {
+        }
     });
 
     podio.SpaceCollection = Backbone.Collection.extend({
-        model: podio.SpaceModel
+        model: podio.SpaceModel,
+        initialize: function(){
+
+        },
+
+        /**
+         *
+         * @param searchTerm
+         * @returns {*}
+         */
+        search: function (searchTerm) {
+            if (searchTerm == "") {
+                return this;
+            }
+
+            var regexp = new RegExp(searchTerm, "gi");
+
+            return _(this.filter(function (model) {
+                return regexp.test(model.get("name"));
+            }));
+
+        }
+
     });
 
     podio.OrganisationModel = Backbone.Model.extend({
-        initialize: function () {},
+        initialize: function () {
+        },
 
         parse: function (response) {
             response.spaces = new podio.SpaceCollection(response.spaces);
@@ -30,17 +54,40 @@ var podio = podio || {};
 
     podio.SpaceView = Backbone.View.extend({
         initialize: function (collection) {
+            var self = this;
+console.log('init');
             this.collection = collection;
+
+            //Ugly ;(
+            $('.menu input').on('keyup', function(){
+                //Pass in the collection
+                self.search(this);
+            });
         },
+
+        //copy of the collection, only showing the results after a filter
+        filteredCollection: '',
 
         template: '<li><a href="{{url}}">{{{name}}}</a></li>',
 
+        search: function(input) {
+
+            //Perform a search
+            this.filteredCollection = this.collection.search( $(input).val() );
+
+        },
         render: function () {
 
             var self = this;
             var output = "";
+            var coll = this.collection;
 
-            this.collection.each(function (model) {
+            if (this.filteredCollection != "") {
+                coll = this.filteredCollection;
+            }
+
+            console.log(coll);
+            coll.each(function (model) {
                 output += Mustache.render(self.template, model.attributes);
             });
 
@@ -58,8 +105,8 @@ var podio = podio || {};
             '<img src="{{image.thumbnail_link}}" alt="{{hosted_by_humanized_name}}">',
             '</div>',
             '<div class="areaContainer">',
-                '<a href="{{url}}">{{{name}}}</a>',
-                '<ol>'
+            '<a href="{{url}}">{{{name}}}</a>',
+            '<ol>'
         ].join(''),
 
         templateAfter: '</ol></div></li>',
@@ -68,7 +115,7 @@ var podio = podio || {};
         output: "",
 
         events: {
-            'click': 'inputKeyup'
+            //'click': 'inputKeyup'
         },
 
         inputKeyup: function () {
@@ -78,10 +125,13 @@ var podio = podio || {};
         initialize: function () {
             var self = this;
             this.collection = new podio.OrganisationCollection();
+
             this.collection.fetch().complete(function () {
                 //when data is fetched, render the view
                 self.render();
             });
+
+
         },
 
         render: function () {
@@ -92,7 +142,7 @@ var podio = podio || {};
                 self.output += Mustache.render(self.templateBefore, model.attributes);
 
                 $(model.attributes.spaces).each(function () {
-                    self.output += new podio.SpaceView(this).render();
+                   // self.output += new podio.SpaceView(this).render();
                 });
 
                 self.output += self.templateAfter;
