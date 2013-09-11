95d1070d9418ab872b104485e116e5abc244a993
Sune Radich Christensen <sykokaj@gmail.com>
2013-09-11 14:30:38 +0200

    Test

--- js/main.js
+++ js/main.js
@@ -25,11 +25,66 @@ var podio = podio || {};
         url: 'data/data.json'
     });
 
-    var data = new podio.OrganisationCollection();
-    data.fetch().complete(function(){
-       setTimeout(function(){
-           console.log(data);
-       },50);
+    podio.OrganisationView = Backbone.View.extend({
+        el: ".menu > div input",
+        template: [
+            '<div>',
+            '<ol>',
+            '{{#areas}}',
+            '<li>',
+            '<div>',
+            '<img src="{{image.thumbnail_link}}" alt="{{hosted_by_humanized_name}}">',
+            '</div>',
+            '<div class="areaContainer">',
+            '<a href="{{url}}">{{{name}}}</a>',
+            '<ol>',
+            '{{#spaces}}',
+            '<li><a href="{{url}}">{{{name}}}</a></li>',
+            '{{/spaces}}',
+            '</ol>',
+            '</div>',
+            '</li>',
+            '{{/areas}}',
+            '</ol>',
+            '</div>'
+        ].join(''),
+
+        //The html that will be appended to the DOM
+        output: "",
+        events: {
+            'click': 'inputKeyup'
+        },
+        inputKeyup: function () {
+            console.log('filt', this);
+
+        },
+        initialize: function () {
+            var self = this;
+            this.collection = new podio.OrganisationCollection();
+            this.collection.fetch().complete(function () {
+                self.render();
+            });
+        },
+        render: function () {
+            var self = this;
+            this.collection.each(function (model) {
+                self.output += Mustache.render(self.template, model.attributes);
+            });
+
+            console.log(this.collection);
+
+            self.$el.after(self.output);
+        }
     });
 
+    var spacesview = new podio.OrganisationView();
+
 }(jQuery));
+
+$('.menu a').on('click', function (event) {
+    //Prevent this click event to bubble up
+    event.stopPropagation();
+    event.preventDefault();
+    $(this).parent().toggleClass('open');
+
+});
